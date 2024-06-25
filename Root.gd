83689e292a4819f7c07365f67d188667015d930c
kumisWasir <testdoan13@gmail.com>
2024-06-25 16:02:27 +0700

    Meet MVp

--- Root.gd
+++ Root.gd
@@ -3,6 +3,9 @@ extends Node2D
 var red_stone_scene = preload("res://stone_red.tscn")
 var blue_stone_scene = preload("res://blue_stone.tscn")
 
+signal winner_found(stone)
+
+var is_winner = false
 var width = 10
 var height = 10
 var current_stone:Node2D 
@@ -30,7 +33,7 @@ func _on_cursor_place_at(area):
 	var area_name:String = ""
 	if current_stone == null:
 		current_stone = red_stone_scene.instantiate()
-	if area.get_child_count() == 1:
+	if area.get_child_count() == 1 && is_winner == false:
 		change_curent_stone()
 		area_name = area.name
 		var array_name = area_name.split();
@@ -105,38 +108,44 @@ func edit_grid(row:int , col:int, data:int):
 
 func get_grid_data(row:int , col:int):
 	grid[9 * row + col]
-#print grid
-func see_grid():
-	print(grid)
 	
+
 func check_win_horizontal():
 	for j in range(0, 9):
 		for i in range(0,6):
 			if grid[9*j +i] == 2 && grid[9*j +i +1] == 2&& grid[9*j +i+2] == 2 && grid[9*j +i+3] == 2:
-				print("Biru Win")
+				is_winner = true
+				winner_found.emit("Blue")
 			if grid[9*j +i] == 1 && grid[9*j +i +1] == 1 && grid[9*j +i+2] == 1 && grid[9*j +i+3] == 1:
-				print("Red Win")
+				is_winner = true
+				winner_found.emit("Red")
 
 func check_win_vertical():
 	for j in range(0, 9):
 		for i in range(0,6):
 			if grid[9*i +j] == 2 && grid[9*(i + 1) + j] == 2&& grid[9*(i + 2) + j] == 2 && grid[9*(i + 3) + j] == 2:
-				print("Biru Win")
+				is_winner = true
+				winner_found.emit("Blue")
 			if grid[9*i +j] == 1 && grid[9*(i + 1) + j] == 1 && grid[9*(i + 2) + j] == 1 && grid[9*(i + 3) + j] == 1:
-				print("Red Win")
+				is_winner = true
+				winner_found.emit("Red")
 
 func check_win_diagonal_left():
 	for j in range(0,6):
 		for i in range(0,6):
 			if grid[9*i +j] == 2 && grid[9*(i + 1) + (j+ 1)] == 2&& grid[9*(i + 2) + (j+ 2)] == 2 && grid[9*(i + 3) + (j+ 3)] == 2:
-				print("Biru Win")
+				is_winner = true
+				winner_found.emit("Blue")
 			if grid[9*i +j] == 1 && grid[9*(i + 1) + (j+ 1)] == 1 && grid[9*(i + 2) + (j+ 2)] == 1 && grid[9*(i + 3) + (j+ 3)] == 1:
-				print("Red Win")
+				is_winner = true
+				winner_found.emit("Red")
 
 func check_win_diagonal_right():
 	for j in range(0,6):
 		for i in range(3,9):
 			if grid[9*j + i] == 2 && grid[9*(j + 1) + (i - 1)] == 2&& grid[9*(j + 2) + (i - 2)] == 2 && grid[9*(j + 3) + (i - 3)] == 2:
-				print("Biru Win")
+				is_winner = true
+				winner_found.emit("Blue")
 			if grid[9*j + i] == 1 && grid[9*(j + 1) + (i - 1)] == 1 && grid[9*(j + 2) + (i - 2)] == 1 && grid[9*(j + 3) + (i - 3)] == 1:
-				print("Red Win")
+				is_winner = true
+				winner_found.emit("Red")
